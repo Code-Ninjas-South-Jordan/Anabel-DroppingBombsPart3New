58a61f9b109123d95df3626f4ccd4ef95758877f
Code Ninjas 4 <codeninjassouthjordan@gmail.com>
2021-03-19 12:06:21 -0600

    asds
    dsdfews

---
+++ AnabelDroppingBombs3Part/Assets/Scripts/GameManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Private Spawner Object
+    private Spawner spawner;
+    //public GameObject title;
+    //private Vector2 screenBounds;
+    //public GameObject playerPrefab;
+    //private GameObject player;
+    //private bool gameStarted = false;
+    //public GameObject splash;
+    //public GameObject scoreSystem;
+    //public Text scoreText;
+    //public int pointsWorth = 1;
+    //private int score;
+    void Start()
+    {
+        spawner.active = false;
+        //title.SetActive(true);
+        //splash.SetActive(false);
+    }
+    void Awake()
+    {
+        spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        //screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        //player = playerPrefab;
+        //scoreText.enabled = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            spawner.active = true;
+            //title.SetActive(false);
+        }
+        //var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
+        /*foreach(GameObject bombObject in nextBomb)
+        {
+            if(bombObject.transform.position.y < (-screenBounds.y) - 12 || !gameStarted)
+            {
+                Destroy(bombObject);
+            } else if(bombObject.transform.position.y < (-SscreenBounds.y) && gameStarted)
+            {
+                scoreSystem.GetComponent<Score>().AddScore(pointsWorth);
+                Destroy(bombObject);
+            }
+        }*/
+        /*if(!gameStarted)
+        {
+            if(Input.anyKeyDown)
+            {
+                ResetGame();
+            }
+        } else
+        {
+            if(!player)
+            {
+                OnPlayerKilled();
+            }
+        }    */
+    
+    }
+    /*void ResetGame()
+    {
+        spawner.active = true;
+        //title.SetActive(false);
+        //player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
+        gameStarted = true;
+        splash.SetActive(false);
+        scoreText.enabled = true;
+        scoreSystem.GetComponent<Score>().score = 0;
+        scoreSystem.GetComponent<Score>().Start();
+    /*void OnPlayerKilled()
+    {
+        spawner.active = false;
+        gameStarted = false;
+        splash.SetActive(true);
+    }*/
+}
---
+++ AnabelDroppingBombs3Part/Assets/Scripts/Spawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Bomb Prefab Game Object
+    public GameObject bombPrefab;
+    //This variable is the delay of how long it takes to spawn the bomb. The value of the delay variable given here is the default.
+    public float delay = 2.0f;
+    //Checks if spawner is active
+    public bool active = true;
+    //Delay range
+    public Vector2 delayRange = new Vector2(1, 2);
+    //The private variable for the screen boundaries.
+    private Vector2 screenBounds;
+    //The private variables for the bombPrefab object's height and width
+    private float objectWidth;
+    private float objectHeight;
+    void Start()
+    {
+        ResetDelay();
+        StartCoroutine(EnemyGenerator());
+    }
+    IEnumerator EnemyGenerator() {
+        yield return new WaitForSeconds(delay);
+        //Random X is the random x coordinate value. Spawn Y is the y value that the object will spawn at. Reset Delay is pretty self explanatory. It basically resets the delay to a random range of the delay range x coordinate as well as y
+        if(active)
+        {
+            float randomX = Random.Range(screenBounds.x - objectWidth, screenBounds.x * -1 + objectWidth);
+            float spawnY = (screenBounds.y + objectHeight) + 5;
+            Instantiate(bombPrefab, new Vector3(randomX, spawnY, 0), bombPrefab.transform.rotation);
+            ResetDelay();
+            screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+            objectWidth = bombPrefab.GetComponent<MeshRenderer>().bounds.size.x / 2;
+            objectHeight = bombPrefab.GetComponent<MeshRenderer>().bounds.size.y / 2;
+
+        }
+        StartCoroutine(EnemyGenerator());
+    }
+    void ResetDelay()
+    {
+        delay = Random.Range(delayRange.x, delayRange.y);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
