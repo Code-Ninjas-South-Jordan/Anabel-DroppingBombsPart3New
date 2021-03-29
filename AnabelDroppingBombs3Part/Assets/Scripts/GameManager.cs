5b30f9cc9b6bae740bcbce21db56b498055fa52e
CodeNinjasSJ <57017893+CodeNinjasSJ@users.noreply.github.com>
2021-03-29 16:46:05 -0600

    New Update

--- AnabelDroppingBombs3Part/Assets/Scripts/GameManager.cs
+++ AnabelDroppingBombs3Part/Assets/Scripts/GameManager.cs
@@ -1,34 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     //Private Spawner Object
     private Spawner spawner;
-    //public GameObject title;
-    //private Vector2 screenBounds;
-    //public GameObject playerPrefab;
-    //private GameObject player;
-    //private bool gameStarted = false;
-    //public GameObject splash;
-    //public GameObject scoreSystem;
-    //public Text scoreText;
-    //public int pointsWorth = 1;
-    //private int score;
+
+    public GameObject title;
+    public GameObject splash;
+
+    private Vector2 screenBounds; //Boundaries of the Screen
+
+    public GameObject playerPrefab;
+    private GameObject player;
+
+    private bool gameStarted = false;
+    public GameObject scoreSystem;
+    public Text scoreText;
+    public int pointsWorth = 1;
+    private int score;
     void Start()
     {
         spawner.active = false;
-        //title.SetActive(true);
-        //splash.SetActive(false);
+        title.SetActive(true);
+        splash.SetActive(false);
     }
     void Awake()
     {
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
-        //screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-        //player = playerPrefab;
-        //scoreText.enabled = false;
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        player = playerPrefab;
+        scoreText.enabled = false;
     }
     // Update is called once per frame
     void Update()
@@ -36,21 +40,21 @@ public class GameManager : MonoBehaviour
         if(Input.anyKeyDown)
         {
             spawner.active = true;
-            //title.SetActive(false);
+            title.SetActive(false);
         }
-        //var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
-        /*foreach(GameObject bombObject in nextBomb)
+        var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
+        foreach(GameObject bombObject in nextBomb)
         {
             if(bombObject.transform.position.y < (-screenBounds.y) - 12 || !gameStarted)
             {
                 Destroy(bombObject);
-            } else if(bombObject.transform.position.y < (-SscreenBounds.y) && gameStarted)
+            } else if(bombObject.transform.position.y < (-screenBounds.y) && gameStarted)
             {
                 scoreSystem.GetComponent<Score>().AddScore(pointsWorth);
                 Destroy(bombObject);
             }
-        }*/
-        /*if(!gameStarted)
+        }
+        if(!gameStarted)
         {
             if(Input.anyKeyDown)
             {
@@ -62,23 +66,27 @@ public class GameManager : MonoBehaviour
             {
                 OnPlayerKilled();
             }
-        }    */
+        }    
     
     }
-    /*void ResetGame()
+    void ResetGame()
     {
         spawner.active = true;
-        //title.SetActive(false);
-        //player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
-        gameStarted = true;
+
+        title.SetActive(false);
         splash.SetActive(false);
+
+        player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
+
+        gameStarted = true;
         scoreText.enabled = true;
         scoreSystem.GetComponent<Score>().score = 0;
         scoreSystem.GetComponent<Score>().Start();
-    /*void OnPlayerKilled()
+    }
+    void OnPlayerKilled()
     {
         spawner.active = false;
         gameStarted = false;
         splash.SetActive(true);
-    }*/
+    }
 }
